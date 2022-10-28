26a51277e748d331842aaaf7b6ddb0a9db100678
Matheus Farias <matheusfarias@g.harvard.edu>
2022-10-28 14:09:23 -0400

    amend

--- MNIST/adcs.py
+++ MNIST/adcs.py
@@ -1,5 +1,5 @@
 import torch
 import numpy as np
 
-adcs = (torch.mul(torch.FloatTensor([8, 4, 5, 6, 6, 6, 6, 7]),np.ones((100,8)))).tolist()
+adcs = (np.dot(np.array([8, 4, 5, 6, 6, 6, 6, 7]),np.ones((100,8)))).tolist()
 print(adcs)
